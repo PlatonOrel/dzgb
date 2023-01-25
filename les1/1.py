581c08d68e8e5c1cb2c9c74ba0a8de1f6254a114
Platon Orel <takezohensai@gmail.com>
2023-01-25 12:09:44 +0300

    Задание 1.1

---
+++ les1/1.py
@@ -0,0 +1,13 @@
+int_a =1
+float_a =2.5
+str_input = str(input("Введите строку: "))
+in_input = int(input("Введите целое число: "))
+
+print (f"""int_a = {int_a}, type = {type(int_a)}""")
+print (f"""int_a = {float_a}, type = {type(float_a)}""")
+
+print(f"Введёная строка {str_input}")
+print(f"Введенное число {in_input}")
+
+
+
