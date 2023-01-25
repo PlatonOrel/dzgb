fd8853e9bcadeef86ad684b5e68052283434b844
Platon Orel <takezohensai@gmail.com>
2023-01-25 12:11:25 +0300

    Задание 1.2

---
+++ les1/2.py
@@ -0,0 +1,14 @@
+#2. Пользователь вводит время в секундах.
+# Переведите время в часы, минуты, секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.
+
+time_sec = int(input('Введите число: '))
+hours = time_sec//3600
+hours_res = (hours) if hours > 10 else ('0' + str(hours))
+minutes = (time_sec % 3600)//60
+minutes_res = (minutes) if minutes > 10 else ('0' + str(minutes))
+seconds = (time_sec % 3600) % 60
+seconds_res = (seconds) if seconds > 10 else ('0' + str(seconds))
+if hours > 24:
+    print('Часов больше чем часов в сутках.')
+else:
+    print(f'Время: {hours_res}:{minutes_res}:{seconds_res}')
